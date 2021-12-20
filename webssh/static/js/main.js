dae0e6ad5bb92c8da13470fda9c3ec8d6d0a3f64
xuwei777 <954157605@qq.com>
2021-12-20 10:23:30 +0800

    修改shell界面样式

--- webssh/static/js/main.js
+++ webssh/static/js/main.js
@@ -179,7 +179,7 @@ jQuery(function($){
     }
 
     // 修改处，修改终端宽度
-    var cols = parseInt(window.innerWidth / style.width - 50, 10) - 1;
+    var cols = parseInt(window.innerWidth / style.width - 30, 10) - 1;
 
     // 修改处，修改终端高度
     var rows = parseInt(window.innerHeight / style.height - 6, 10);
